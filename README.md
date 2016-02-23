fb13617a29006e52b562cdab2788e5d6abbf7553
Dave Parfitt <dparfitt@chef.io>
2016-02-23 16:21:15 -0500

    you know

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # simlink
+
+Does `ln -s` bring the phrase [\<lotion brand\>, take me away](https://www.youtube.com/watch?v=sNgZFxAnKtA) to mind?
+
+If so, then try simlink! Simlink doesn't care which order you specify arguments! All you need is a file/directory that exists, and one that doesn't (yet!). Isn't this so very exciting!
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,35 @@ use clap::{Arg, App};
 use std::path::Path;
 use std::process::Command;
 
-fn link(existing_file: &str, new_link:&str) {
-    println!("Linking {} to {}", existing_file, new_link);
+fn link(existing_file: &str, new_link:&str) -> () {
+    //println!("Linking {} to {}", existing_file, new_link);
 
     let status = Command::new("ln")
         .arg("-s")
         .arg(existing_file)
         .arg(new_link)
         .status().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
+        panic!("failed to execute process, bad process! {}", e)
     });
 
-    println!("process exited with: {}", status);
+    match status.code() {
+        Some(c) => std::process::exit(c),
+        // not sure if I care about the signal info here, do you?
+        None => panic!("Empty status code, killed by a signal? Ouch!")
+    };
+}
 
+fn exit_for_humans(msg: &str, status: i32) -> () {
+    // ie, don't print panic() garbage
+    println!("{}", msg);
+    std::process::exit(status);
 }
 
 fn main() {
 
     let matches = App::new("simlink")
                       .version("1.0")
-                      .author("Dave Parfitt <diparfitt@gmail.com>")
-                      .about("ln -s, go away")
+                      .about("ln -s, meow/miaow")
                       .arg(Arg::with_name("A")
                                .help("Maybe a file, maybe a link you want to make?")
                                .required(true)
@@ -37,27 +45,27 @@ fn main() {
     // A + B are required, clap will exit before unwrap() might fail
     let file_a = matches.value_of("A").unwrap();
     let file_b = matches.value_of("B").unwrap();
-    println!("Value for config: {} {}", file_a, file_b);
 
     let path_a = Path::new(file_a);
     let path_b = Path::new(file_b);
 
-    let sa = match path_a.to_str() {
-        Some(s) => s,
-        None => panic!("Invalid filename for <A>")
+    if let None = path_a.to_str() {
+        exit_for_humans("Invalid filename for <A>", -1);
     };
 
-    let sb = match path_b.to_str() {
-        Some(s) => s,
-        None => panic!("Invalid filename for <B>")
+    if let None = path_b.to_str() {
+        exit_for_humans("Invalid filename for <B>", -1);
     };
+
+    let sa = path_a.to_str().unwrap();
+    let sb = path_b.to_str().unwrap();
 
     let paths = (path_a.exists(), path_b.exists());
     match paths {
         (true, false) => link(sa, sb),
         (false, true) => link(sb, sa),
-        (true, true) => panic!("Both files already exist"),
-        (false, false) => panic!("Neither file exists")
+        (true, true) => exit_for_humans("Both paths exist!", -1),
+        (false, false) => exit_for_humans("Neither path exists", -1)
     };
 
 }
